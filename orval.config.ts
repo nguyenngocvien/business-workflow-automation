afc950e48418ddf0f5abc0c276b1eec9e820755e
Vien Nguyen <vien.nguyen.12067@gmail.com>
2026-05-26 11:56:18 +0700

    update name and path for each openapi generated

--- orval.config.ts
+++ orval.config.ts
@@ -1,43 +1,64 @@
-const sharedAxiosMutator = './src/lib/orvalMutator.ts';
-
 export default {
   identity: {
     input: './openapi/identity/openapi.json',
+
     output: {
       target: './src/api/identity/index.ts',
       client: 'axios',
+
       override: {
-        mutator: sharedAxiosMutator,
+        mutator: {
+          path: './src/lib/orvalMutator.ts',
+          name: 'orvalMutator',
+        },
       },
     },
   },
+
   document: {
     input: './openapi/document/openapi.json',
+
     output: {
       target: './src/api/document/index.ts',
       client: 'axios',
+
       override: {
-        mutator: sharedAxiosMutator,
+        mutator: {
+          path: './src/lib/orvalMutator.ts',
+          name: 'orvalMutator',
+        },
       },
     },
   },
+
   workflow: {
     input: './openapi/workflow/openapi.json',
+
     output: {
       target: './src/api/workflow/index.ts',
       client: 'axios',
+
       override: {
-        mutator: sharedAxiosMutator,
+        mutator: {
+          path: './src/lib/orvalMutator.ts',
+          name: 'orvalMutator',
+        },
       },
     },
   },
+
   connector: {
     input: './openapi/connector/openapi.json',
+
     output: {
       target: './src/api/connector/index.ts',
       client: 'axios',
+
       override: {
-        mutator: sharedAxiosMutator,
+        mutator: {
+          path: './src/lib/orvalMutator.ts',
+          name: 'orvalMutator',
+        },
       },
     },
   },
